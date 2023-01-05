f596440a11648852b8ab17dd5a53be39bc8d50b3
gilrezin <gil.rezin@hotmail.com>
2023-01-04 17:32:04 -0800

    Mobile UI Tweaks
    UI enlarged for mobile devices

--- Lab Project - Rezin/Assets/Scripts/Globals.cs
+++ Lab Project - Rezin/Assets/Scripts/Globals.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public static class Globals
 {
-    public const int MOBILE_SCALING = 1; // 1 is PC, 2 is mobile
+    public const int MOBILE_SCALING = 2; // 1 is PC, 2 is mobile
     public const int MENU_BUTTON_BOUNDS = 150 * MOBILE_SCALING;
 }
